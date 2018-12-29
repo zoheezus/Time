ef4a0fd394e7cb2fcde6ef9aed10ab815d2f15e7
zoheezus <37160795+zoheezus@users.noreply.github.com>
2018-12-29 12:52:53 -0500

    Update time.js

--- time.js
+++ time.js
@@ -1,25 +1,25 @@
-const date = new Date();               
+var date = new Date();               
 
-function calculateTime(city, offset) {
+function calcTime(city, offset) {
     utc = date.getTime() + (date.getTimezoneOffset() * 60000);
     newDate = new Date(utc + (3600000 * offset));
     return city + newDate.toLocaleTimeString();
 }
 
-module.exports = {
-    calculateTime
-}
+//new york time
+console.log(calcTime('New York ', '-5'));
 
-// //new york time
-// console.log(calculateTime('New York ', '-5'));
+//cali time
+console.log(calcTime('California ', '-8'));
 
-// //cali time
-// console.log(calculateTime('California ', '-8'));
-
-// //colorodo time
-// console.log(calculateTime('Colorodo ', '-7'));
+//colorodo time
+console.log(calcTime('Colorodo ', '-7'));
 
 // [-5, 'America/New_York'],
 // [-8, 'America/California'],
 // [-7, 'America/Colorodo'],
 // [0, 'GMT']
+
+module.exports = {
+    calcTime
+}
