05faadbd8ddcc510a70bbd50202c5cceeaba7743
zoheezus <37160795+zoheezus@users.noreply.github.com>
2018-12-29 12:52:23 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,30 @@
 const https = require('https');
 
-var express = require('express');
-var port = process.env.PORT || 8080;
-var time = require('./time');
-var app = express();
+var express = require('express'),
+    port = process.env.PORT || 8080,
+    time = require('./time'),
+    app = express();
 
 app.get('/', function (req, res, next) {
     res.write('Hello, World!\n');
-    res.write(time.calculateTime('\nNew York: ', '-5'));
-    res.write(time.calculateTime('\nColorado: ', '-8'));
-    res.write(time.calculateTime('\nCalifornia: ', '-7'));
+    res.write(time.calcTime('\nNew York: ', '-5'));
+    res.write(time.calcTime('\nColorado: ', '-8'));
+    res.write(time.calcTime('\nCalifornia: ', '-7'));
     res.end();
 });
 
-app.listen(port, () => { //use function () if err
+//delete when finished
+app.listen(port, () => {
     console.log('listening on port 8080');
 });
+
+// if (https.redirect == true) {
+//     app.get('/', function (req, res, next) {
+//         res.write('Hello, World!\n');
+//         res.write(time.calcTime('\nNew York: ', '-5'));
+//         res.write(time.calcTime('\nColorado: ', '-8'));
+//         res.write(time.calcTime('\nCalifornia: ', '-7'));
+//         res.end();
+//     });    
+//     console.log("page was reloaded");
+// }
